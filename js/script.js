f29c4d92849f467b4ff7be0c19f9ea6d6e3d33cc
mariegrace31 <mariegracebmg@gmail.com>
2023-05-16 10:50:23 +0200

    Modify css file due to linters errors

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
-const hamburger = document.getElementById ('hamburger');
+const hamburger = document.getElementById('hamburger');
 const closing = document.getElementById('closingIcon');
 const links = document.querySelectorAll('.mobileLinks');
 const menu = document.querySelector('.nav-menu');
 const body = document.querySelector('body');
 
-// menu mobile function 
+// menu mobile function
 
 hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    body.classList.toggle('active');
+  menu.classList.toggle('active');
+  body.classList.toggle('active');
 });
 
 closing.addEventListener('click', () => {
-    menu.classList.toggle('active');
+  menu.classList.toggle('active');
 });
 
 // this funcition is for mobile functionality
 links.forEach((link) => {
-    link.addEventListener('click', () => {
-        menu.classList.toggle('active');
-        body.classList.toggle('active');
-    })
+  link.addEventListener('click', () => {
+  menu.classList.toggle('active');
+  body.classList.toggle('active');
+  });
 });
