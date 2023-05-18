8ef00722db5b00d65af436a146f918384afeea44
moFuhidy <mo7ammed9290@gmail.com>
2023-05-18 12:17:11 +0300

    get the data form local storage

--- js/storage-data.js
+++ js/storage-data.js
@@ -1,6 +1,7 @@
-const email =document.getElementById('email');
-const Fullname= document.getElementById('fullname');
-const textArea=document.getElementById('textarea');
+const form = document.querySelector('form');
+const email = document.getElementById('email');
+const Fullname = document.getElementById('fullname');
+const textArea = document.getElementById('textarea');
 function savedData() {
   const formData = {
     fullname: Fullname.value,
@@ -11,3 +12,13 @@ function savedData() {
 }
 
 form.addEventListener('submit', savedData);
+
+// get the item from the local storage 
+window.addEventListener('load', () => {
+  const dataStorage = JSON.parse(localStorage.getItem('formData'));
+  if (dataStorage) {
+    email.value = dataStorage.email;
+    Fullname.value = dataStorage.fullname;
+    textArea.value = dataStorage.textarea;
+  }
+});
